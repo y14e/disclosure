388e49b23487734deb8c503e6a723e9e8a3c18b6
Yusuke Kamiyamane <y@y14e.com>
2026-08-29 16:29:00 +0900

    v2.0.9

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
  * WAI-ARIA compliant disclosure pattern implementation in TypeScript.
  * Using the <details> and <summary> element.
  *
- * @version 2.0.8
+ * @version 2.0.9
  * @author Yusuke Kamiyamane
  * @license MIT
  * @copyright Copyright (c) Yusuke Kamiyamane
